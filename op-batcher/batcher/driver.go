1c7a0508f34b07202ec752d9b53629a0a3124b8e
Albert <albert@rss3.io>
2024-04-08 08:51:21 +0800

    feat: check blob data after finality

--- op-batcher/batcher/driver.go
+++ op-batcher/batcher/driver.go
@@ -1,6 +1,7 @@
 package batcher
 
 import (
+	"bytes"
 	"context"
 	"encoding/hex"
 	"errors"
@@ -448,10 +449,14 @@ func (l *BatchSubmitter) submitBlobToNearDA(data []byte) ([]byte, error) {
 	time.Sleep(5 * time.Second)
 	// finality is achieved which is 3 blocks (around 2-3 seconds) its not possible for a reorg to happen
 	// check the submitted blob after finality
-	_, err = l.NearDAClient.Client.Get(maybeFrameRef, 0)
+	blobData, err := l.NearDAClient.Client.Get(maybeFrameRef, 0)
 	if err != nil {
 		log.Error("failed to get data from near, maybe chain reorg", "id", hex.EncodeToString(data), "err", err)
 		return nil, err
+	}
+	if bytes.Equal(blobData, data) {
+		log.Error("failed to get data from near, blob data mismatch", "id", hex.EncodeToString(maybeFrameRef), "expected", hex.EncodeToString(data), "got", hex.EncodeToString(blobData))
+		return nil, fmt.Errorf("submitted data mismatch with obtained data, id :%s", hex.EncodeToString(maybeFrameRef))
 	}
 
 	return maybeFrameRef, nil
